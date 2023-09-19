f18d28ee8f664bddecd24e50027d730b6709aec7
akugone <dev@akumaw.com>
2023-09-19 16:52:22 +0200

    prettier

--- tests/storage-utils.ts
+++ tests/storage-utils.ts
@@ -1,15 +1,15 @@
-import { newMockEvent } from "matchstick-as"
-import { ethereum } from "@graphprotocol/graph-ts"
-import { DataSet } from "../generated/Storage/Storage"
+import { newMockEvent } from "matchstick-as";
+import { ethereum } from "@graphprotocol/graph-ts";
+import { DataSet } from "../generated/Storage/Storage";
 
 export function createDataSetEvent(data: string): DataSet {
-  let dataSetEvent = changetype<DataSet>(newMockEvent())
+  let dataSetEvent = changetype<DataSet>(newMockEvent());
 
-  dataSetEvent.parameters = new Array()
+  dataSetEvent.parameters = new Array();
 
   dataSetEvent.parameters.push(
     new ethereum.EventParam("data", ethereum.Value.fromString(data))
-  )
+  );
 
-  return dataSetEvent
+  return dataSetEvent;
 }
